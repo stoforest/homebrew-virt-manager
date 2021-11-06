1c142eb734ca95059d183783d5bef720e83dc160
stoforest <2010shizesen@gmail.com>
2021-11-07 01:02:15 +0800

    Update virt-manager.rb

--- virt-manager.rb
+++ virt-manager.rb
@@ -3,8 +3,8 @@ class VirtManager < Formula
 
   desc "App for managing virtual machines"
   homepage "https://virt-manager.org/"
-  url "https://virt-manager.org/download/sources/virt-manager/virt-manager-3.2.0.tar.gz"
-  sha256 "2b6fe3d90d89e1130227e4b05c51e6642d89c839d3ea063e0e29475fd9bf7b86"
+  url "https://virt-manager.org/download/sources/virt-manager/virt-manager-3.0.0.tar.gz"
+  sha256 "8afb58b1ee979e6ba9bdda3f98bd553b35981519660a649b38309d7924662f3d"
   revision 3
 
   depends_on "intltool" => :build
